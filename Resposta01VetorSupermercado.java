3e5cbc02089ba168a8564b2c77bb66ef0723df56
Juliano Duarte <juliano.mimura.duarte@gmail.com>
2026-05-05 09:11:48 -0300

    05/05

--- Resposta01VetorSupermercado.java
+++ Resposta01VetorSupermercado.java
@@ -1,14 +1,14 @@
 class Resposta01VetorSupermercado {
     public static void main(String[] args) {
         float[] caixa = new float[5];
-        caixa[0] = 1000;
-        caixa[1] = 1200;
-        caixa[2] = 2000;
-        caixa[3] = 3200;
-        caixa[4] = 3100;
+        caixa[0] = 1000f;
+        caixa[1] = 1200f;
+        caixa[2] = 2000f;
+        caixa[3] = 3200f;
+        caixa[4] = 3100f;
 
         // total vendido no dia
-        float total = 0;
+        float total = 0f;
         for (int i = 0; i  < caixa.length; i++) {
             total += caixa[i];
         }
@@ -19,24 +19,24 @@ class Resposta01VetorSupermercado {
 
         // qual caixa teve a maior venda
         float maiorValor = caixa[0];
-        float maiorCaixa = 0;
+        float maiorCaixa = 0f;
         for (int i = 1; i  < caixa.length - 1; i++) {
-            maiorCaixa = (caixa[i] > maiorValor) ? i : 0;
+            maiorCaixa = (caixa[i] > maiorValor) ? i : 0f;
             maiorValor = (caixa[i] > maiorValor) ? caixa[i] : maiorValor;
         }
 
         // qual caixa teve a menor venda
         float menorValor = caixa[0];
-        float menorCaixa = 0;
+        float menorCaixa = 0f;
         for (int i = 1; i  < caixa.length - 1; i++) {
-            menorCaixa = (caixa[i] < menorValor) ? i : 0;
+            menorCaixa = (caixa[i] < menorValor) ? i : 0f;
             menorValor = (caixa[i] < menorValor) ? caixa[i] : menorValor;
         }
 
         System.out.printf("Total do dia: R$%.2f\n", total);
         System.out.printf("Media por caixa: R$%.2f\n", media);
-        System.out.printf("Maior venda: Caixa %d com R$%.2f\n", maiorCaixa, maiorValor);
-        System.out.printf("Menor venda: Caixa %d com R$%.2f\n", menorCaixa, menorValor);
+        System.out.printf("Maior venda: Caixa %.0f com R$%.2f\n", maiorCaixa, maiorValor);
+        System.out.printf("Menor venda: Caixa %.0f com R$%.2f\n", menorCaixa, menorValor);
 
     }
 }
